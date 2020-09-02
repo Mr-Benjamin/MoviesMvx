e194b256e48356d463ef19a1c03bc0dee4a71fd7
Mr-Benjamin <28747033+Mr-Benjamin@users.noreply.github.com>
2020-09-02 23:45:34 +1200

    Scafolded the movie model

---
+++ Data/MoviesMvxContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoviesMvx.Models
+{
+    public class MoviesMvxContext : DbContext
+    {
+        public MoviesMvxContext (DbContextOptions<MoviesMvxContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoviesMvx.Models.Movie> Movie { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MoviesMvx.Models;
 
 namespace MoviesMvx
 {
@@ -33,6 +35,9 @@ namespace MoviesMvx
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<MoviesMvxContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MoviesMvxContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
