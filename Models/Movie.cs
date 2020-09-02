ccb6f59d3ff36216cc02697a0579af6a258e254b
Mr-Benjamin <28747033+Mr-Benjamin@users.noreply.github.com>
2020-09-02 23:30:37 +1200

    Added data type date annotation to ReleaseDate Propertyin movie class

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -10,6 +10,7 @@ namespace MoviesMvx.Models
     {
         public int Id { get; set; }
         public string  Title{ get; set; }
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate{ get; set; }
         public string Genre{ get; set; }
         public decimal Price{ get; set; }
